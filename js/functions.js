eac545737d1a4454f5e6f3406846a00a14a7269f
JulietMind <32484424+JulietMind@users.noreply.github.com>
2017-10-05 12:59:53 +0200

    added part of menu

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,5 @@
-// $(document).ready(function(){
-
-// 	$(".img-menu").on("click", function(){
-
-// 		$("content-menu").toggleClass("show");
-// 	});
-
-// });
-
 $(document).ready(function(){
-	$('burguer').on('click',function(){
-		$('header nav ol').toggleClass('show');
+	$('.img-menu').on('click',function(){
+		$('header nav ul').toggleClass('show');
 	})
 })
